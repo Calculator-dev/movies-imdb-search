a8d0e972c7e566d176d80f3cba02c46e70042232
Haris <haris_aksalic@hotmail.com>
2022-02-11 14:31:41 +0100

    Deleted Hello button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -46,7 +46,6 @@ export default function SearchBar() {
                         onChange={onChange}
                     />
                 </Search>
-                <Button>Hello</Button>
             </form>
         </div>
     )
